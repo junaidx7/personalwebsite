7e8074f6fd4fc3417cbb6922d639389b8ee652ab
Junaid <Play@Junaids-MacBook-Pro.local>
2024-11-09 22:02:31 +0400

    bigbox changes

--- src/components/Home/head.js
+++ src/components/Home/head.js
@@ -14,7 +14,7 @@ const Head = () => {
 
     useEffect(() => {
 
-        
+
         const timer = setTimeout(() => {
             const primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--primary');
             const a1 = annotate(document.querySelector('#e1'), { type: 'circle', color: '#FFCA59', iterations: 5 });
@@ -37,33 +37,28 @@ const Head = () => {
             <div className='maxwidth-container-withflex'>
 
                 <div className='bigbox'>
-                    
+
                     <h1 className="bigtext " style={{ fontFamily: '"Inter Tight", sans-serif' }}> <span id="e1" className='pop'>Junaid</span> designs<br />intuitive products.</h1>
 
                     <div className='tiltedvid'>
-                    <video className='vidinside' src={prizigner} playsInline autoPlay muted loop ></video>
+                        <video className='vidinside' src={prizigner} playsInline autoPlay muted loop ></video>
                     </div>
 
                 </div>
 
 
-                <div>
-
                 <h5 className="subtext-under-bigtext" id="bio">
                     Hello, I am a <span id='e2'>  Lead UI UX designer based in dubai with 7yrs of experience</span> in crafting
                     user-centric web and mobile solutions across diverse industries.
                 </h5>
-                
-                <div className='designingfordiv top-2xl'>
+
+
+                <div className='designingfordiv'>
                     <h5 className='subtext-under-bigtext'>Currently designing for</h5>
                     <img src={duicon} alt="Description" />
                     <h5 className='subtext-under-bigtext'>telecom</h5>
                     <img src={openarrow} alt="Description" />
                 </div>
-
-
-                </div>
-
 
 
 
