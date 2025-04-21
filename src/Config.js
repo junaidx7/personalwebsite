129ab5af4e9f7d3b611e633be35258c5fe25f8b3
Junaid <junaid79x@gmail.com>
2025-04-21 19:57:52 +0400

    changed resume link

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 const Config = {
-  resumeLink: 'https://docs.google.com/document/d/126Vq7aX_T0-lE94Y6QAzkcWc7PvHhXtlx8Z0De7IkQI/edit?usp=sharing',
+  resumeLink: 'https://drive.google.com/file/d/115e0pWaQSrutzFk8UGOFQYuqpojEfjAx/view?usp=sharing',
   twitterLink: 'https://www.x.com',
   linkedinLink: 'https://www.linkedin.com/in/productdesignerjunaid/',
   whatsapp: 'https://wa.me/971589789768',
